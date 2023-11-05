cb773e74ae75f890a187852de1b9df02bbc25265
feitan <ilya.shkoda06@gmail.com>
2023-11-05 13:43:32 +0000

    v1.0.1 item-completed starting-auth-reg

--- item/models.py
+++ item/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Category(models.Model):
@@ -14,6 +15,20 @@ class Category(models.Model):
     
     
 class Item(models.Model):
+  category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
   name = models.CharField(max_length=255)
   description = models.TextField(blank=True, null=True)
+  price = models.FloatField()
+  image = models.ImageField(upload_to='item_images', blank=True, null=True)
+  is_sold = models.BooleanField(default=False)
+  created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  
+  def __str__(self):
+    return self.name
+  
+  class Meta:
+    verbose_name = 'item'
+    verbose_name_plural = 'items'
  
